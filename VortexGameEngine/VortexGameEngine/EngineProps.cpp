7cf06b333973694b8bb1d01d9885b5871a6b441f
Mattie <mattiemonster1@hotmail.com>
2017-09-11 18:14:27 +0100

    Dev 2

---
+++ VortexGameEngine/VortexGameEngine/EngineProps.cpp
@@ -0,0 +1,16 @@
+#include "EngineProps.h"
+
+void PrintEngineName()
+{
+	std::cout << "Engine: Vortex Engine" << std::endl;
+}
+
+std::string GetEngineName()
+{
+	return std::string("Vortex Engine");
+}
+
+const char * GetEngineNameChar()
+{
+	return "Vortex Engine";
+}
--- VortexGameEngine/VortexGameEngine/EngineProps.h
+++ VortexGameEngine/VortexGameEngine/EngineProps.h
@@ -1,6 +1,10 @@
 #ifndef ENGINE_PROPS_H_
 #define ENGINE_PROPS_H_
 
+#include <iostream>
 
+static void PrintEngineName();
+static std::string GetEngineName();
+static const char* GetEngineNameChar();
 
 #endif
